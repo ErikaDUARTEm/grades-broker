c5e9d8b6b51a172c1b61cbb2c82d8d2801ebeebd
erika.duarte <erika.duarte.mora@gmail.com>
2025-02-27 13:33:10 -0500

    configuration rabbitMQ

--- src/main/java/com/example/gradesconsumer/config/RabbitMQConfiguration.java
+++ src/main/java/com/example/gradesconsumer/config/RabbitMQConfiguration.java
@@ -1,4 +1,20 @@
 package com.example.gradesconsumer.config;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQConfiguration {
+  private static final String QUEUE_NAME = "ecommerce_queue";
+
+  @Bean
+  public Queue queue() {
+    return new Queue(QUEUE_NAME);
+  }
+  @Bean
+  public Jackson2JsonMessageConverter messageConverter(){
+    return new Jackson2JsonMessageConverter();
+  }
 }
